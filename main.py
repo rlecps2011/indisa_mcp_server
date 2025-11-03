0db90f6a81b2c67b2d64f1c6f7142a9dd7a21acf
rlecps2011 <rlecps2011@gmail.com>
2025-11-03 14:59:42 -0300

    123

--- main.py
+++ main.py
@@ -24,3 +24,7 @@ def get_status():
 @app.get("/")
 def root():
     return {"status": "ok", "mensagens": ["get_mensagem", "get_status"]}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
